046f56a37aeaabadefe602d020e79967698856d8
Jessica Orrick <jessica.orrick@outlook.com>
2023-10-16 19:10:22 -0500

    lecture updates

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const Cereal = require('./cereal.js');
 const Milk = require('./milk.js');
 const Dessert = require('./dessert.js');
 const Meal = require('./meal.js');
+
+
+// let mondayBowl = new Cereal ("Cinnamon Toast Crunch", ["Cinnamon", "Toast", "Sugar", "Crunch", "MSG"], 8.95, true, false)
+
+// console.log(mondayBowl)
+
+// let mondayMilk = new Milk("Prarie Farms", "Whole Orange Sherbert Nails", 42.00, true);
+
+// let mondayDessert = new Dessert("Yes")
+
+// let mondayMeal = new Meal("Dinner I guess", mondayBowl, mondayMilk, mondayDessert);
+
+// console.log(mondayMeal.eatUp())
+
+let cutiePatootie = new Eevee("Patootie", "A Concept", 2, ["HyperBeam", "Tail Whip", "Baby-Doll Eyes"]);
+
+console.log(cutiePatootie.attack())
--- meal.js
+++ meal.js
@@ -12,7 +12,7 @@ class Meal {
 
     eatUp() {
         return `Good morning! It's time for the most important meal of the day: ${this.name}! Please enjoy a nice, big bowl of ${this.cereal.brand} cereal with ${this.milk.flavor} flavored milk poured all over it! It's absolutely delicious! No, I am not being held at gunpoint to say these lines. By the way, did you know that your cereal contains ${this.cereal.ingredients.join(", ")}? It's great! If you've still got room in your tummy, enjoy a nice bite of ${this.dessert.type}. Have a wonderful day. 🙂`;
-      }
+    }
 }
 
 module.exports = Meal;
